8917801c7be1c9c2fee6b9f64f4b551ec3b34208
Sthevan Bello Alves <sthevan.alves@gmail.com>
2021-04-26 20:42:28 -0300

    Alura - Asp.Net Core MVC - Initializing course 01

---
+++ Alura.ListaLeitura.App/Startup.cs
@@ -0,0 +1,29 @@
+﻿using Alura.ListaLeitura.App.Repositorio;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+
+namespace Alura.ListaLeitura.App
+{
+    public class Startup
+    {
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Run(LivrosParaLer);
+        }
+
+        public Task LivrosParaLer(HttpContext context)
+        {
+                        
+
+            var _repo = new LivroRepositorioCSV();
+            
+            var repoString = _repo.ParaLer.ToString();
+
+            return context.Response.WriteAsync(repoString);
+
+        }
+    }
+}
